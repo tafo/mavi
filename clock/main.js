87ee64d3f2705d93cc593072b53980168fd78c7c
tafo <tayfunyirdem@gmail.com>
2022-04-02 13:18:59 +0300

    clock folder is created

--- clock/main.js
+++ clock/main.js
@@ -1,19 +1,19 @@
 (function () {
   function setClock() {
-    let date = new Date();
+    const date = new Date();
 
-    let second = date.getSeconds();
-    let secondDegree = (second / 60) * 360;
+    const second = date.getSeconds();
+    const secondDegree = (second / 60) * 360;
     const secondHand = document.querySelector(".secondhand");
     secondHand.style.transform = `rotate(${secondDegree}deg)`;
 
-    let minute = date.getMinutes();
-    let minuteDegree = (minute / 60) * 360 + (second / 60) * 6;
+    const minute = date.getMinutes();
+    const minuteDegree = (minute / 60) * 360 + (second / 60) * 6;
     const minuteHand = document.querySelector(".minutehand");
     minuteHand.style.transform = `rotate(${minuteDegree}deg)`;
 
-    let hour = date.getHours();
-    let hourDegree = (hour / 12) * 360 + (minute / 60) * 6;
+    const hour = date.getHours();
+    const hourDegree = (hour / 12) * 360 + (minute / 60) * 6;
     const hourHand = document.querySelector(".hourhand");
     hourHand.style.transform = `rotate(${hourDegree}deg)`;
   }
